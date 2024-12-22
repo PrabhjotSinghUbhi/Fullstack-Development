df1ed9552c1ed3f7e655828815dcaf962701ec5b
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2024-12-22 15:49:23 +0000

    datatypes explored

--- JAVASCRIPT-CRASH-COURSE/01_Basics/02_dataTypes.js
+++ JAVASCRIPT-CRASH-COURSE/01_Basics/02_dataTypes.js
@@ -2,9 +2,27 @@
 
 // alert("3" + 3) we are using node.js not browser's console.
 
+let name = "Prabhjot Singh";
+let age = 18;
+let consent = "hello";
+let isLoggedIn = true;
 
+let x = null // This is not undefined this is null.
+let y; // This is undefined.
 
+// number = 2 to the power 53
+// bitInt
+// String => " "
+// boolean => true or false
+// null => stand alone value
+//undefined
 
+// Object. eg. Null is a Object.
 
-
+console.log(name + " " +"is "+ typeof name);
+console.log(age + " " +"is  "+ typeof age);
+console.log(consent + " " +"is  "+ typeof consent);
+console.log("isLoggedIn" +"is   "+ " " + typeof isLoggedIn);
+console.log("x" + " " +"is  "+ typeof null);
+console.log("y" + " " +"is  "+ typeof y);
 
