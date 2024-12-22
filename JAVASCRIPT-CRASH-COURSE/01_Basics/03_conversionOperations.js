51690efcbf374d449d315af5bb71220bf4a0a7c6
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2024-12-22 18:43:28 +0000

    operators

--- JAVASCRIPT-CRASH-COURSE/01_Basics/03_conversionOperations.js
+++ JAVASCRIPT-CRASH-COURSE/01_Basics/03_conversionOperations.js
@@ -21,9 +21,36 @@ let isLoggedIn = ""
 
 let convertInBoolean = Boolean(isLoggedIn)
 
-console.log(typeof convertInBoolean);
+// console.log(typeof convertInBoolean);
 
-console.log(convertInBoolean);
+// console.log(convertInBoolean);
 
 // 0 ==> false: 1 ==> true
 // "" ==> false : "non empty string" ==> true
+
+// ******************** OPERATIONS **************************
+
+let value = 3
+let negValue = - value
+// console.log(negValue);
+/*
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**3);
+console.log(2/3);
+console.log(2%3);
+*/
+
+let str1 = "Prabh"
+let str2 = "awesome"
+
+let str3 = str1 +" " + str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+
+console.log(1+ 2 + "2" );
+console.log(+"");
+
