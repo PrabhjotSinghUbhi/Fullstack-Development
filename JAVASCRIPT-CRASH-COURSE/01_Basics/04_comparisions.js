298701c88b266af6c5066439a0dcf4556250b90a
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2024-12-22 18:58:50 +0000

    Comparisions

---
+++ JAVASCRIPT-CRASH-COURSE/01_Basics/04_comparisions.js
@@ -0,0 +1,13 @@
+// console.log(2 > 1);
+
+
+console.log(null > 0)
+console.log(null < 0)
+console.log(null <= 0)
+
+// DISCLAIMER - avoid comparing variables of different datatypes.
+
+console.log("2" == 2)
+// '===' will also check their datatypes.
+console.log("2" === 2)
+
