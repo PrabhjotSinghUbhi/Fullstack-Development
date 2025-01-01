b3d34e816283fb1c2500535c4294c7c8a9272d28
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-01-01 22:48:24 +0530

    Basics of Function done

---
+++ Javascript-By-Hitesh-Sir/03_Basics/01_functions.js
@@ -0,0 +1,34 @@
+// function sayMyname(){
+//     console.log('H')
+//     console.log('I')
+//     console.log('S')
+//     console.log('E')
+//     console.log('N')
+//     console.log('B')
+//     console.log('E')
+//     console.log('R')
+//     console.log('G')
+// }
+
+// function addTwoNum(num1,num2){
+//     // console.log(num1+num2)
+// }
+
+// // let result = addTwoNum(5,5)
+// // console.log(result);
+
+// function addTwoNum(num1,num2){ 
+//     return num1 + num2
+// }
+
+// let result = addTwoNum(4,90)
+// console.log(result);
+
+
+
+
+function calculateCartPrices(val1,val2,...num1){
+    return num1
+}
+
+console.log(calculateCartPrices(100,200,300,499))
