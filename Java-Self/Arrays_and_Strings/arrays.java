630a0fb013903c4c685584c716265db6c69f1b6b
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-03-03 21:26:50 +0530

    java started

---
+++ Java-Self/Arrays_and_Strings/arrays.java
@@ -0,0 +1,12 @@
+class Arrays_and_Strings {
+    public static void main(String[] args) {
+        // ? Arrays
+        int[] arr = new int[5];
+        for (int index = 0; index < arr.length; index++) {
+            System.out.print(arr[index] + " ");
+        }
+
+        //! Now lets about Strings in java.
+        
+    }
+}
