92829e3e8da8211e8df088457d94672ef1685323
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-03-05 00:13:02 +0530

    Strings and arrys complete leetcode remaining

---
+++ Java-Self/Arrays_and_Strings/Learn_StringBuilders.java
@@ -0,0 +1,19 @@
+public class Learn_StringBuilders {
+    public static void main(String[] args) {
+        StringBuilder s1 = new StringBuilder();
+        for (int i = 0; i < 26; i++) {
+            s1.append((char) ('a' + i));
+        }
+        System.out.println(s1);
+
+        s1.deleteCharAt(0);
+        System.out.println(s1);
+
+        s1.reverse();
+        System.out.println(s1);
+
+        for (int i = 0; i < args.length; i++) {
+            
+        }
+    }
+}
