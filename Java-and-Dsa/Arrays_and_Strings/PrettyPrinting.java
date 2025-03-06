36869cb99a92c0070aea3baaabcca45496d5a5f8
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-03-06 11:18:26 +0530

    sliding window done

--- Java-and-Dsa/Arrays_and_Strings/PrettyPrinting.java
+++
@@ -1,9 +0,0 @@
-public class PrettyPrinting {
-    public static void main(String[] args) {
-        float a = 23.337899f;
-        System.out.printf("Formatted number: %.2f", a);
-        System.out.println();
-
-        System.out.printf("PI is : %.3f", Math.PI);
-    }
-}
---
+++ Java-and-Dsa/Backend/Arrays_and_Strings/PrettyPrinting.java
@@ -0,0 +1,9 @@
+public class PrettyPrinting {
+    public static void main(String[] args) {
+        float a = 23.337899f;
+        System.out.printf("Formatted number: %.2f", a);
+        System.out.println();
+
+        System.out.printf("PI is : %.3f", Math.PI);
+    }
+}
