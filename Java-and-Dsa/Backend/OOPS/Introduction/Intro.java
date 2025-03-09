5d3ad05007e95f0f9e5e696257c0d78238fdeec6
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-03-09 22:40:29 +0530

    Basic class creation done

--- Java-and-Dsa/Backend/OOPS/Introduction/Intro.java
+++ Java-and-Dsa/Backend/OOPS/Introduction/Intro.java
@@ -1,13 +1,21 @@
 class Intro {
     public static void main(String[] args) {
 
-        // new keyword is usd
+        // new keyword is used to create new objects.
         Student Prince = new Student();
+        Prince.rollNo = 24;
+        System.out.println(Prince.rollNo);
+        // If values are not assigned the default values are printed.
+
+        System.out.println(Prince.name);
+        System.out.println(Prince.marks);
+        System.out.println(Prince.yes);
     }
 
     static class Student {
         int rollNo;
         String name;
         float marks;
+        boolean yes;
     }
 }
