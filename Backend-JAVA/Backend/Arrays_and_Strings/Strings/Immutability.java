41a7af23abd16d975a5368880ed5de4925724972
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-03-16 01:07:16 +0530

    motivated

--- Backend-JAVA/Backend/Arrays_and_Strings/Strings/Immutability.java
+++
@@ -1,9 +0,0 @@
-// package Strings;
-public class Immutability {
-    public static void main(String[] args) {
-        String s1 = "Java";
-        String s2 = s1.concat(" Programming");
-        System.out.println(s1);
-        System.out.println(s2);
-    }
-}
--- Backend-JAVA/Backend/Arrays_and_Strings/Strings/Operators.java
+++
@@ -1,8 +0,0 @@
-
-import java.util.ArrayList;
-
-public class Operators {
-    public static void main(String[] args) {
-        System.out.println((56 + "" + new ArrayList<>()));
-    }
-}
