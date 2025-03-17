3a2a1cf2e8f3218908eafa5bd0485dd720d20668
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-03-17 23:03:00 +0530

    jagged array Learned

--- core-java-spring-microservices/coreJava/Demo.java
+++ core-java-spring-microservices/coreJava/Demo.java
@@ -1,17 +1,5 @@
 class Demo {
     public static void main(String[] args) {
-
-        calc cu = new calc();
-        cu.add();
-    }
-}
-
-class calc {
-    int num1;
-    int num2;
-
-    public int add() {
-        System.out.println(this.num1 + this.num2);
-        return 4;
+         
     }
 }
---
+++ core-java-spring-microservices/jaggedArrays.java
@@ -0,0 +1,20 @@
+public class jaggedArrays {
+    public static void main(String[] args) {
+
+        int[][] arr = new int[3][]; // This is a jagged array where one of the size is not mentioned
+
+        // But you have to give the size individually.
+        arr[0] = new int[4];
+        arr[1] = new int[2];
+        arr[2] = new int[3];
+
+        for (int n[] : arr) {
+
+            for (int j : n) {
+                System.out.print(j + " ");
+            }
+            System.out.println();
+        }
+
+    }
+}
