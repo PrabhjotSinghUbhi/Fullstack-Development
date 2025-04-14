76af02508ac6b07673ed5b17a1df8b2d17aee27f
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-04-14 14:08:48 +0530

    Background Changer made using React.

--- React-Js-By-Hitesh-Sir/Counter/counter/src/App.jsx
+++ React-Js-By-Hitesh-Sir/Counter/counter/src/App.jsx
@@ -2,12 +2,48 @@ import { useState } from "react";
 
 function App() {
 
+  // use of useState() hook
   let [count, setCount] = useState(0)
 
   const addValue = () => {
     console.log("clicked", count);
     
-    setCount(count = count + 1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+    setCount(count +=  1)
+  }
+
+  const reduceValue = () => {
+    console.log("clicked", count);
+
+    if(count > 0) setCount(count -= 1)
+
+    
   }
 
   return(
@@ -17,30 +53,25 @@ function App() {
               Prabhjot Singh's Counter 
       </div>
 
-      <div>
+      <div className="items-center flex flex-col">
   
         <button 
-            className=" h-12 p-2 border w-fit rounded-lg m-5 active:bg-amber-500 font-bold active:text-black" onClick={addValue} id="btn" >
-            counter <span className="ml-3 text-red-200">
-                {count}
-            </span>
+            className=" h-12 p-4 flex items-center border w-fit rounded-lg m-5 active:bg-amber-500 font-bold active:text-black bg-green-600" onClick={addValue} id="btn" >
+            Increment
         </button>
-
 
         <button 
-            className=" h-12 p-2 border w-fit rounded-lg m-5 active:bg-amber-500 font-bold active:text-black" onClick={addValue} id="btn" >
-            counter <span className="ml-3 text-red-200">
-                {count}
-            </span>
+            className=" h-12 p-4 flex items-center border w-fit rounded-lg bg-red-900 text-white m-5 active:bg-amber-500 font-bold active:text-black" onClick={reduceValue} id="btn" >
+            Decrement
         </button>
 
 
-        <button 
-            className=" h-12 p-2 border w-fit rounded-lg m-5 active:bg-amber-500 font-bold active:text-black" onClick={addValue} id="btn" >
+        <div 
+            className="flex justify-center items-center text-4xl p-10 h-12 border w-fit rounded-lg mt-5 active:bg-amber-500 font-bold active:text-black" id="btn" >
             counter <span className="ml-3 text-red-200">
                 {count}
             </span>
-        </button>
+        </div>
 
       </div>
 
