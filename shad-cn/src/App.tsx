1a6e4c210bca71475ff298ba924e6352ca822c36
Prabhjot Singh <prabh.ubhi7042@gmail.com>
2025-09-13 17:23:36 +0530

    things have changed.

---
+++ shad-cn/src/App.tsx
@@ -0,0 +1,11 @@
+import Login from "./components/Login/Login";
+
+function App() {
+  return (
+    <div className="flex flex-wrap justify-center items-center h-screen gap-2 md:flex-row">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
